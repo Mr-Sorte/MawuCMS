b046404e5aaf49c72cc9ce1fb3e85b1ee8d9a040
Wulles <lhdsa@icloud.com>
2021-01-13 12:38:23 -0300

    Update cms_maintenance.sql

--- Sqls/cms_mantenimiento.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `cms_mantenimiento`;
-CREATE TABLE `cms_mantenimiento` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `mantenimiento` enum('0','1') DEFAULT '0',
-  `motivo` longtext NOT NULL,
-  `timestamp` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
-INSERT INTO `cms_mantenimiento` VALUES (1,'0','','');
