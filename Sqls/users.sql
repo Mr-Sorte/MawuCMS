72fa930d7031cd99db0b9c4d2b71e2843bcbf16c
Wulles <lhdsa@icloud.com>
2021-01-14 17:14:53 -0300

    Update users.sql

--- Sqls/users.sql
+++ Sqls/users.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `users` ADD `passcode` VARCHAR(65) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `password`;
 ALTER TABLE `users` ADD `theme` VARCHAR(255) NOT NULL DEFAULT 'light' AFTER `rank`;
 ALTER TABLE `users` ADD `visible` ENUM('0', '1') NOT NULL DEFAULT '1' AFTER `online`;
 ALTER TABLE `users` ADD `premiar` INT(11) NOT NULL DEFAULT '0' AFTER `points`;
