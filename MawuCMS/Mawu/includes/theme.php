0c039b17510876ad427da919f24558a12e381c79
Skorp <76567301+Itisyan@users.noreply.github.com>
2021-01-18 17:10:25 +0100

    Add files via upload

---
+++ MawuCMS/Mawu/includes/theme.php
@@ -0,0 +1,35 @@
+<?php if(isset($Holo)) { ?>
+<html lang="<?php echo $Holo['htmllang']; ?>">  
+<?php if(Loged == FALSE) { ?>
+<script>
+    var themed = new Date();
+    var themeh = themed.getHours();
+
+    if(themeh > <?php echo $Holo['debut_auto_dark']; ?>-1 || themeh < <?php echo $Holo['fin_auto_dark']; ?>-1){
+        document.write('<html data-theme="dark">');
+    } else {
+		document.write('<html data-theme="light">');
+	};
+</script>
+<?php } elseif(Loged == TRUE) { 
+$theusertheme = $myrow['theme'];
+if($theusertheme == "auto") { ?>
+<script>
+    var themed = new Date();
+    var themeh = themed.getHours();
+
+    if(themeh > <?php echo $Holo['debut_auto_dark']; ?>-1 || themeh < <?php echo $Holo['fin_auto_dark']; ?>-1){
+        document.write('<html data-theme="dark">');
+    } else {
+		document.write('<html data-theme="light">');
+	};
+</script>
+<?php } else { ?>
+<html data-theme="<?php echo $myrow['theme']; ?>">
+<?PHP } } ?>
+<noscript><meta http-equiv="refresh" content="3; URL='/script_error'"/></noscript>
+	    
+<?PHP } else { 
+    header("Location: /");
+    exit; 
+             } ?>
